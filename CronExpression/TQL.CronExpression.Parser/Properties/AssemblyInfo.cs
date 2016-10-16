da25c8f05bc7a59a201784e55ee8d3ba5650e5a8
Jakub Puchała <puchala.czwa2@hotmail.com>
2016-10-16 21:43:40 +0200

    increase version

--- CronExpression/TQL.CronExpression.Parser/Properties/AssemblyInfo.cs
+++ CronExpression/TQL.CronExpression.Parser/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b8735922-c602-44ee-9df5-45679e2a1436")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
--- CronExpression/TQL.CronExpression.Visitors/Properties/AssemblyInfo.cs
+++ CronExpression/TQL.CronExpression.Visitors/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d3cbf7c1-87c6-4500-85c5-3d7d4580c309")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
